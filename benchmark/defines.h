89e16757e9019c8fae0e35e35c1283120d316450
Paul Springer <springer@aices.rwth-aachen.de>
2017-09-27 07:46:41 +0200

    added missing defines.h

---
+++ benchmark/defines.h
@@ -0,0 +1,11 @@
+#include <complex>
+
+using FloatComplex = std::complex<float>;
+using DoubleComplex = std::complex<double>;
+
+typedef float floatType;
+//typedef double floatType;
+//typedef DoubleComplex floatType;
+//typedef FloatComplex floatType;
+
+
